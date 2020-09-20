1df839bff902c03995c86d5dcb58b8a3b86d874f
samakshsood1 <54793136+samakshsood1@users.noreply.github.com>
2020-09-21 02:54:51 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,20 @@
+package typeCasting;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+//		byte x = 4;
+//		
+//		int y = x;
+//		System.out.println(x);
+//		System.out.println(y);
+//		
+		double myDouble = 3.14;
+		
+		int myInt = (int)myDouble;
+		System.out.println(myInt);
+		System.out.println(myDouble);
+
+	}
+
+}
