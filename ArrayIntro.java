9214a7a5aae2c50d37488b4ea3255e50d51b92f4
samakshsood1 <54793136+samakshsood1@users.noreply.github.com>
2020-09-21 02:57:17 +0530

    Add files via upload

---
+++ ArrayIntro.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class ArrayIntro {
+
+	public static void main(String[] args) {
+		int[] marks = new int[5]; // dataType[] arrayName = new datType[n]
+		//int marks[] = new int[5]; is also correct but not when we are declaring more than one arrays
+		// ex: int marks[], grade, here the marks will be declared as array but the grade will be declared as simple int
+		
+	}
+
+}
