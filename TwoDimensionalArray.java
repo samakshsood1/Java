452a1de2098eb782507be6dae024cd17311613ab
samakshsood1 <54793136+samakshsood1@users.noreply.github.com>
2020-09-21 02:58:32 +0530

    Add files via upload

---
+++ TwoDimensionalArray.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+int a[] [] = new int [5] [3];// 5 arrays with three values which means 5 rows and 3 columns.
+// To access 2d arrays we have to specify the index of both rows and columns in sq brackets
+//a [1] [2] = 23;
+//System.out.println(a[1] [2]);
+//initialising a 2d array:
+int b[][] = {{1,1,1,1,1},
+             {2,2,2,2,2},
+             {3,3,3,3,3,3}};
+}
+
+}
